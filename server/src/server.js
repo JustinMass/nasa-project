920cc38bae2dd5d8e935433f109674f4922c72ae
Justin Massey <justin.massey@auctane.com>
2023-04-30 16:04:36 -0700

    Setup planets router, controller in express

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,11 @@
-const PORT = 8000;
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
