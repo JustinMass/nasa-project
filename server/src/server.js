dea7309d175681ab84c9da4824f928811442d687
Justin Massey <justin.massey@auctane.com>
2023-05-05 11:48:17 -0700

    persisted planets in mongo

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+MONGO_URL =
+  'mongodb+srv://nasa-api:fFWxoP4BfO6mD9Hu@cluster0.xk4rdlr.mongodb.net/nasa';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
