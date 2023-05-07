8a0ab0a75aaad35d13fb4fec9e4b95249984af1c
Justin Massey <justin.massey@auctane.com>
2023-05-06 17:31:23 -0700

    connected launches and tests to mongo

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,17 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
+
+const { mongoConnect } = require('./services/mongo');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-MONGO_URL =
-  'mongodb+srv://nasa-api:fFWxoP4BfO6mD9Hu@cluster0.xk4rdlr.mongodb.net/nasa';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
